b8631ba15072d84adc775fa34ba2582f6dc4729e
Martin Leandro Tablang <mtablang@gmail.com>
2024-11-21 18:37:58 +0900

    UDP Video POC

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,18 +1,75 @@
 <script lang="ts">
-  import { invoke } from "@tauri-apps/api/core";
+  import { invoke } from '@tauri-apps/api/core';
+  import { onMount } from 'svelte';
+  import { bind, send, unbind } from '@kuyoonjo/tauri-plugin-udp';
+  import { listen } from '@tauri-apps/api/event';
 
-  let name = $state("");
-  let greetMsg = $state("");
+  import { readFile, BaseDirectory } from '@tauri-apps/plugin-fs';
 
-  async function greet(event: Event) {
-    event.preventDefault();
-    // Learn more about Tauri commands at https://tauri.app/develop/calling-rust/
-    greetMsg = await invoke("greet", { name });
-  }
+  // let name = $state('');
+  // let greetMsg = $state('');
+
+  let videoElement: HTMLVideoElement; // Reference to the <video> element
+
+  // async function greet(event: Event) {
+  //   event.preventDefault();
+  //   // Learn more about Tauri commands at https://tauri.app/develop/calling-rust/
+  //   greetMsg = await invoke('greet', { name });
+  // }
+
+  onMount(async () => {
+    const id = 'unique-id';
+    await bind(id, '0.0.0.0:8080');
+    // await send(id, '192.168.1.2:9090', 'hello');
+    // await unbind(id);
+
+    await listen('plugin://udp', (x) => {
+      const videoTitle = String.fromCharCode(...x.payload.data);
+
+      playVideo(videoTitle);
+    });
+  });
+
+  const playVideo = async (videoTitle: string) => {
+    const file = await readFile(`${videoTitle}.mp4`, {
+      baseDir: BaseDirectory.Desktop,
+    });
+
+    // Convert binary data to a Blob URL
+    const blob = new Blob([new Uint8Array(file)], { type: 'video/mp4' });
+
+    if (videoElement) {
+      videoElement.src = URL.createObjectURL(blob);
+
+      console.log(`videoElement.src`, videoElement.src);
+
+      videoElement
+        .play()
+        .catch((err) => {
+          console.error('video playback error:', err);
+        })
+        .then(() => {
+          console.log('video playback success');
+          videoElement.requestFullscreen();
+        });
+    }
+  };
+  const enterFullscreen = () => {
+    if (videoElement && videoElement.requestFullscreen) {
+      videoElement.requestFullscreen().catch((err) => {
+        console.error('Fullscreen error:', err);
+      });
+    } else {
+      console.error('Fullscreen API is not supported.');
+    }
+  };
 </script>
 
 <main class="container">
-  <h1>Welcome to Tauri + Svelte</h1>
+  <video bind:this={videoElement}> </video>
+  <button onclick={enterFullscreen}>Go Fullscreen</button>
+
+  <!-- <h1>Welcome to Tauri + Svelte</h1>
 
   <div class="row">
     <a href="https://vitejs.dev" target="_blank">
@@ -29,128 +86,127 @@
 
   <form class="row" onsubmit={greet}>
     <input id="greet-input" placeholder="Enter a name..." bind:value={name} />
-    <button type="submit">Greet</button>
+    <button type="submit">Greet___</button>
   </form>
-  <p>{greetMsg}</p>
+  <p>{greetMsg}</p> -->
 </main>
 
 <style>
-.logo.vite:hover {
-  filter: drop-shadow(0 0 2em #747bff);
-}
+  .logo.vite:hover {
+    filter: drop-shadow(0 0 2em #747bff);
+  }
 
-.logo.svelte-kit:hover {
-  filter: drop-shadow(0 0 2em #ff3e00);
-}
+  .logo.svelte-kit:hover {
+    filter: drop-shadow(0 0 2em #ff3e00);
+  }
 
-:root {
-  font-family: Inter, Avenir, Helvetica, Arial, sans-serif;
-  font-size: 16px;
-  line-height: 24px;
-  font-weight: 400;
-
-  color: #0f0f0f;
-  background-color: #f6f6f6;
-
-  font-synthesis: none;
-  text-rendering: optimizeLegibility;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  -webkit-text-size-adjust: 100%;
-}
-
-.container {
-  margin: 0;
-  padding-top: 10vh;
-  display: flex;
-  flex-direction: column;
-  justify-content: center;
-  text-align: center;
-}
-
-.logo {
-  height: 6em;
-  padding: 1.5em;
-  will-change: filter;
-  transition: 0.75s;
-}
-
-.logo.tauri:hover {
-  filter: drop-shadow(0 0 2em #24c8db);
-}
-
-.row {
-  display: flex;
-  justify-content: center;
-}
-
-a {
-  font-weight: 500;
-  color: #646cff;
-  text-decoration: inherit;
-}
-
-a:hover {
-  color: #535bf2;
-}
-
-h1 {
-  text-align: center;
-}
-
-input,
-button {
-  border-radius: 8px;
-  border: 1px solid transparent;
-  padding: 0.6em 1.2em;
-  font-size: 1em;
-  font-weight: 500;
-  font-family: inherit;
-  color: #0f0f0f;
-  background-color: #ffffff;
-  transition: border-color 0.25s;
-  box-shadow: 0 2px 2px rgba(0, 0, 0, 0.2);
-}
-
-button {
-  cursor: pointer;
-}
-
-button:hover {
-  border-color: #396cd8;
-}
-button:active {
-  border-color: #396cd8;
-  background-color: #e8e8e8;
-}
-
-input,
-button {
-  outline: none;
-}
-
-#greet-input {
-  margin-right: 5px;
-}
-
-@media (prefers-color-scheme: dark) {
   :root {
-    color: #f6f6f6;
-    background-color: #2f2f2f;
+    font-family: Inter, Avenir, Helvetica, Arial, sans-serif;
+    font-size: 16px;
+    line-height: 24px;
+    font-weight: 400;
+
+    color: #0f0f0f;
+    background-color: #f6f6f6;
+
+    font-synthesis: none;
+    text-rendering: optimizeLegibility;
+    -webkit-font-smoothing: antialiased;
+    -moz-osx-font-smoothing: grayscale;
+    -webkit-text-size-adjust: 100%;
+  }
+
+  .container {
+    margin: 0;
+    padding-top: 10vh;
+    display: flex;
+    flex-direction: column;
+    justify-content: center;
+    text-align: center;
+  }
+
+  .logo {
+    height: 6em;
+    padding: 1.5em;
+    will-change: filter;
+    transition: 0.75s;
+  }
+
+  .logo.tauri:hover {
+    filter: drop-shadow(0 0 2em #24c8db);
+  }
+
+  .row {
+    display: flex;
+    justify-content: center;
+  }
+
+  a {
+    font-weight: 500;
+    color: #646cff;
+    text-decoration: inherit;
   }
 
   a:hover {
-    color: #24c8db;
+    color: #535bf2;
+  }
+
+  h1 {
+    text-align: center;
   }
 
   input,
   button {
-    color: #ffffff;
-    background-color: #0f0f0f98;
+    border-radius: 8px;
+    border: 1px solid transparent;
+    padding: 0.6em 1.2em;
+    font-size: 1em;
+    font-weight: 500;
+    font-family: inherit;
+    color: #0f0f0f;
+    background-color: #ffffff;
+    transition: border-color 0.25s;
+    box-shadow: 0 2px 2px rgba(0, 0, 0, 0.2);
+  }
+
+  button {
+    cursor: pointer;
+  }
+
+  button:hover {
+    border-color: #396cd8;
   }
   button:active {
-    background-color: #0f0f0f69;
+    border-color: #396cd8;
+    background-color: #e8e8e8;
   }
-}
 
+  input,
+  button {
+    outline: none;
+  }
+
+  #greet-input {
+    margin-right: 5px;
+  }
+
+  @media (prefers-color-scheme: dark) {
+    :root {
+      color: #f6f6f6;
+      background-color: #2f2f2f;
+    }
+
+    a:hover {
+      color: #24c8db;
+    }
+
+    input,
+    button {
+      color: #ffffff;
+      background-color: #0f0f0f98;
+    }
+    button:active {
+      background-color: #0f0f0f69;
+    }
+  }
 </style>
